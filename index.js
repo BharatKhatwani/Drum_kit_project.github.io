014045c503ebb5d0058e13d633a5e3ad16250d35
BharatKhatwani <bharatkhatwani06@gmail.com>
2023-12-25 18:50:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+var noofDrumButton = document.querySelectorAll(".drum").length;
+for(var i = 0; i <noofDrumButton; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+var buttonInnerHtml = this.innerHTML;
+
+makeSound(buttonInnerHtml);
+buttonAnimation(buttonInnerHtml);
+
+});
+document.addEventListener("keypress",function(){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function makeSound(key){
+    switch(key){
+        case "w":
+    var tom1 = new Audio('sounds/tom-1.mp3');  // in js ./ are not a method to access the folder ini it
+    tom1.play();
+    break;
+    
+    
+    case "a":
+        var tom2 = new Audio('sounds/tom-2.mp3');  // in js ./ are not a method to access the folder ini it
+        tom2.play();
+        break;
+    
+        case "s":
+        var tom3 = new Audio('sounds/tom-3.mp3');  // in js ./ are not a method to access the folder ini it
+        tom3.play();
+        break;
+    
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');  // in js ./ are not a method to access the folder ini it
+            tom4.play();
+            break;
+    
+            case "j":
+                var snare = new Audio('sounds/snare.mp3');  // in js ./ are not a method to access the folder ini it
+                snare.play();
+                break;
+        
+                case "k":
+                    var crash = new Audio('sounds/crash.mp3');  // in js ./ are not a method to access the folder ini it
+                    crash.play();
+                    break;
+            
+                    case "l":
+                    var kick = new Audio('sounds/kick-bass.mp3');  // in js ./ are not a method to access the folder ini it
+                    kick.play();
+                    break;
+            
+     default: console.log(buttonInnerHtml);
+    }
+}
+function buttonAnimation(currentkey){
+    var activebutton= document.querySelector("." + currentkey);
+    activebutton.classList.add("pressed");   // its is css shadow maker check and key press button into shadow press in it 
+    // set timout ( first parameter,second parameter);
+    // f p =function are passing in it 
+    // s p = timer in the function in it 
+    setTimeout(function(){
+        activebutton.classList.remove("pressed");
+    } , 100)
+}
+}
